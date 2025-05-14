46fa75d26a1d6818e3e778a46d71c10cdd62fefd
simrankkkk <mansimran.kaur@student.uts.edu.au>
2025-05-14 17:42:10 +1000

    Update print.py

--- print.py
+++ print.py
@@ -1,6 +1,6 @@
 from clearml import Task
 
-task = Task.get_task(task_id="542d5b64c9a447e39963f59e9f17fda9")
+task = Task.get_task(task_id="e23037ffa9414ae5ae89c2cbf6e4f666")
 scalars = task.get_reported_scalars()
 
 from pprint import pprint
